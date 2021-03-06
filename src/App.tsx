549ebe643dee2e38900815ddbcb8e1756b19454b
Ujjwal Kumar <katanadeveloper01@gmail.com>
2021-03-06 22:41:48 +0530

    setup gql basic fetch

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
   const Client = createClient({
     url: 'https://api.github.com/graphql',
     fetchOptions: () => {
-        const token = '6a826c4d3ac3c1a6525c53a9fd79440dbc7d5519';
+        const token = '17d90f17a2b0faab006581629b9617166170d548';
         return {
             headers: {authorization: `bearer ${token}`}
         }
@@ -20,8 +20,8 @@ function App() {
     <Provider value={Client}>
       <Switch>
         <Route path="/" component={Home} exact />
-        <Route path="/projects" component={Projects} />
         <Route path="/projects/:id" component={ProjectId}/>
+        <Route path="/projects" component={Projects} />
       </Switch>
     </Provider>
   );
--- src/projects.tsx
+++ src/projects.tsx
@@ -1,11 +1,57 @@
 import React from 'react';
+import { useQuery } from 'urql';
 
+interface Data {
+    viewer: {
+        name: String;
+        repositories: {
+            nodes: Repo[];
+        }
+    }
+    organization: {
+        name: String;
+        url: String;
+        description: String;
+    }
+}
+
+interface Repo {
+    name: String;
+    description: String;
+}
 
 function Projects() {
+    const ProjectsQuery = `
+    query($number_of_repos: Int!){
+        viewer{
+            name
+            repositories(last: $number_of_repos){
+                nodes{
+                    name
+                    description
+                }
+            }
+        }
+        organization(login: "hackarmour"){
+            name
+            url
+            description
+        }
+    }
 
- return (
-     <h1>Projects</h1>
- )
+`
+    const [result, reexecuteQuery] = useQuery({
+        query: ProjectsQuery,
+        variables: {number_of_repos: 100}
+    })
+
+    const { fetching, error } = result;
+    const data: Data = result.data;
+    if (fetching) return <p>Loading...</p>;
+    if (error) return <p>Oh no... {error.message}</p>;
+   return(
+       <div></div>
+   )
 }
 
 
