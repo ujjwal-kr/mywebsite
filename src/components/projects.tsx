004934e49e8c8207c19b96688802dcabe16be50a
ujjwal <katanadeveloper01@gmail.com>
2021-10-24 06:51:23 +0530

    add epictracker to projects

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -21,3 +21,24 @@ export const ShowcasePara = styled.p`
         font-size: 1.1rem !important;
     }
 `
+
+export const Project = styled.div`
+    padding: 1rem;
+`
+
+export const ProjectList = styled.div`
+    padding-top: 2rem;
+
+`
+
+
+export const ProjectHead = styled.h1`
+    
+`
+
+export const ProjectDec = styled.p`
+    padding-top: .5rem;
+    @media all and (max-width: 700px) {
+        font-size: 1.1rem !important;
+    }
+`
--- src/projects.tsx
+++ src/projects.tsx
@@ -1,17 +1,27 @@
-import { Header, Showcase, ShowcasePara } from "./components/projects"
+import { Header, Showcase, ShowcasePara, ProjectList, ProjectHead, ProjectDec, Project } from "./components/projects"
 
 function Projects() {
-    return(
+    return (
         <div>
             <Header className="has-text-centered">
                 <h1 className="has-text-primary stiff is-size-1	">ProJEcts</h1>
             </Header>
-            
+
             <Showcase>
                 <ShowcasePara className="text is-size-4">
                     Welcome to my projects page. I make lot of stuff in my freetime and a most of it could be found on <a href="https://github.com/ujjwal-kr">my GitHub</a>. Here are some of the notable ones:
                 </ShowcasePara>
+
+                <ProjectList>
+                    <Project>
+                        <ProjectHead><a className="text is-size-3" href="https://github.com/ujjwal-kr/epictracker">EpicTracker</a></ProjectHead>
+                        <ProjectDec className="text is-size-5">A demo of how can I track you using fingerprinting and some automated lookups and stuff, using modern Javascript APIs to identify device metadata and creating a SHA1 hash and sending it to the server so that the device can be recognized later. Here is a <a href="https://epictracker.vercel.app">Live demo</a> </ProjectDec>
+                        <br />
+                    </Project>
+                </ProjectList>
             </Showcase>
+
+
         </div>
     )
 }
