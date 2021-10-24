b0e9dbdc1444ff83841cc405c62b436952b71672
ujjwal <katanadeveloper01@gmail.com>
2021-10-24 06:18:27 +0530

    add project heading and layout

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,5 +1,23 @@
 import styled from 'styled-components'
 
 export const Header = styled.div`
-    text-align: center;
+    padding: .9rem;
+    background-color: rgba(0,0,0, .3)
+`
+
+export const Showcase = styled.section`
+    padding-top: 5rem;
+    width: 80%;
+    margin: auto;
+    
+    @media all and (max-width: 700px){
+        width: 100%;
+        padding: 1rem;
+    }
+`
+
+export const ShowcasePara = styled.p`
+    @media all and (max-width: 700px){
+        font-size: 1.1rem !important;
+    }
 `
--- src/projects.tsx
+++ src/projects.tsx
@@ -1,11 +1,17 @@
-import { Header } from "./components/projects"
+import { Header, Showcase, ShowcasePara } from "./components/projects"
 
 function Projects() {
     return(
         <div>
-            <Header>
+            <Header className="has-text-centered">
                 <h1 className="has-text-primary stiff is-size-1	">ProJEcts</h1>
             </Header>
+            
+            <Showcase>
+                <ShowcasePara className="text is-size-4">
+                    Welcome to my projects page. I make lot of stuff in my freetime and a most of it could be found on <a href="https://github.com/ujjwal-kr">my GitHub</a>. Here are some of the notable ones:
+                </ShowcasePara>
+            </Showcase>
         </div>
     )
 }
