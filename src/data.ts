9062358128ac3b0bbda8d2c06e6b669ddf42ab06
ujjwal <katanadeveloper01@gmail.com>
2021-10-27 05:47:47 +0530

    render projects to projects component

--- src/data.ts
+++ src/data.ts
@@ -1,19 +1,22 @@
-export interface Project {
+export interface IProject {
     title: string
     body: string
+    source?: string
     live?: string
 }
 
-export const Projects: Project[] = [
+export const projectData: IProject[] = [
     {
         title: 'EpicTracker',
+        source: 'https://github.com/ujjwal-kr/epictracker',
         body: 'A demo of how can I track you using fingerprinting and some automated lookups and stuff, using modern Javascript APIs to identify device metadata and creating a SHA1 hash and sending it to the server so that the device can be recognized later',
-        live: 'https://epictracker.vercel.com'
+        live: 'https://epictracker.vercel.app'
     },
 
     {
         title: 'EpicTracker',
+        source: 'https://github.com/ujjwal-kr/epictracker',
         body: 'A demo of how can I track you using fingerprinting and some automated lookups and stuff, using modern Javascript APIs to identify device metadata and creating a SHA1 hash and sending it to the server so that the device can be recognized later',
-        live: 'https://epictracker.vercel.com'
+        live: 'https://epictracker.vercel.app'
     }
 ]
--- src/projects.tsx
+++ src/projects.tsx
@@ -1,29 +1,63 @@
+import { IProject, projectData } from "./data"
 import { Header, Showcase, ShowcasePara, ProjectList, ProjectHead, ProjectDec, Project } from "./components/projects"
+import React from "react"
 
-function Projects() {
-    return (
-        <div>
+interface State {
+    projects?: IProject[]
+}
+
+class Projects extends React.Component<any, State> {
+    constructor(props: any) {
+        super(props)
+        this.state = {
+            projects: []
+        }
+    }
+
+    componentDidMount() {
+        this.setState({
+            projects: projectData
+        })
+    }
+
+    render() {
+        return (
+            <div>
             <Header className="has-text-centered">
                 <h1 className="has-text-primary stiff is-size-1	">ProJEcts</h1>
             </Header>
 
             <Showcase>
-                <ShowcasePara className="text is-size-4">
+            <ShowcasePara className="text is-size-4">
                     Welcome to my projects page. I make lot of stuff in my freetime and a most of it could be found on <a href="https://github.com/ujjwal-kr">my GitHub</a>. Here are some of the notable ones:
-                </ShowcasePara>
-
+            </ShowcasePara>
                 <ProjectList>
-                    <Project>
-                        <ProjectHead><a className="text is-size-3" href="https://github.com/ujjwal-kr/epictracker">EpicTracker</a></ProjectHead>
-                        <ProjectDec className="text is-size-5">A demo of how can I track you using fingerprinting and some automated lookups and stuff, using modern Javascript APIs to identify device metadata and creating a SHA1 hash and sending it to the server so that the device can be recognized later. Here is a <a href="https://epictracker.vercel.app">Live demo</a> </ProjectDec>
-                        <br />
-                    </Project>
+                {this.state.projects ?
+                    this.state.projects.map((item, i) => {
+                        return <Project key={i}>
+                                {item.source ?
+                                    <ProjectHead><a className="text is-size-3" href={item.source}>{item.title}</a></ProjectHead>
+                                :
+                                    <ProjectHead className="text is-size-3">{item.title}</ProjectHead>
+                                }
+                            <ProjectDec className="text is-size-5">
+                                {item.body}
+                            </ProjectDec>
+                            <br />
+                            {item.live ?
+                                 <a href={item.live} className="button is-primary">Live Demo</a> :null
+                            }
+                            <br />
+                        </Project>
+                    })
+                :
+                "Something went wrong"
+                }
                 </ProjectList>
             </Showcase>
-
-
         </div>
-    )
+        )
+    }
 }
 
 export default Projects
