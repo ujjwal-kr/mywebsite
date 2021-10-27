631d84b28722e4d1e9d1585539f4701c10d98058
ujjwal <katanadeveloper01@gmail.com>
2021-10-27 06:56:50 +0530

    add scroll to top

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import './App.sass';
-import {Route, Switch} from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Home from './home';
 import Projects from './projects';
+import ScrollToTop from './scroll'
 
 function App() {
   return (
+    <Fragment>
+      <ScrollToTop />
       <Switch>
         <Route path="/" component={Home} exact />
         <Route path="/projects" component={Projects} />
       </Switch>
+    </Fragment>
+
   );
 }
 
