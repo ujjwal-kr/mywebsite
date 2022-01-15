24cf4d5ed2613cd146d835f061e41141589b8281
Ujjwal Kumar <epicujjwal@protonmail.com>
2022-01-15 23:05:38 +0530

    add ram

--- src/data.ts
+++ src/data.ts
@@ -24,6 +24,12 @@ export const projectData: IProject[] = [
         live: {text: 'Visit', url: "https://positivevibes.vercel.app"}
     },
     {
+        title: 'Ram',
+        source: 'https://github.com/ujjwal-kr/ram',
+        body: 'I made my first programming language (stack based) using Rust. Can perform basic math and conditional jumps. Will be improving it to use some variable abstractions.'
+        live: {text: 'Source', url :"https://github.com/ujjwal-kr/ram"}
+    },
+    {
         title: 'EpicTracker',
         source: 'https://github.com/ujjwal-kr/epictracker',
         body: 'A demo of how can I track you using fingerprinting and some automated lookups and stuff, using modern Javascript APIs to identify device metadata and creating a SHA1 hash and sending it to the server so that the device can be recognized later',
